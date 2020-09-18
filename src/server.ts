272f45d5404ec6f05e2e3c06976026397bd9b601
Tchedre TCHEIN <tchedre.tchein@gmail.com>
2020-09-19 01:02:12 +0200

    Project: add filtered image endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,23 +13,37 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
+    let { image_url } = req.query;
 
-  /**************************************************************************** */
+    // check image url is not empty
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image url is required`);
+    }
+    
+    try {
 
-  //! END @TODO1
+      // download, filter, and save the filtered image locally
+      const filteredpath = await filterImageFromURL(image_url)
+
+      res.status(200).sendFile(filteredpath, {}, (err) => {
+        
+        if (err) { 
+          return res.status(422).send(`Not able to process the image`); 
+        }
+        
+        // Deletes any files on the server
+        deleteLocalFiles([filteredpath]);
+      })
+
+    } catch (err) {
+      res.status(422).send(`Not able to process the image, Make sure that image url is correct`);
+    }
+
+  } );
   
   // Root Endpoint
   // Displays a simple message to the user
